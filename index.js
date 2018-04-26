1ac3e706c5fa8e7c86a6d312f4a347927d7bc59f
Cphil31 <cecilephilippe31@gmail.com>
2018-04-26 15:20:54 +0200

    ajout des test

---
+++ index.js
@@ -0,0 +1,5 @@
+var scene = document.getElementById('scene');
+var titre = document.getElementById('titre');
+
+var parallaxInstance = new Parallax(scene);
+var parallaxInstance = new Parallax(titre);
